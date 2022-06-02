941751a8e27c68ac287b524d5d3425ac84ea4ba6
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2022-06-02 16:18:02 +0200

    basic first page desktop only done.

---
+++ src/components/PageOne.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function PageOne () {
+  return (
+    <div className="page-one">
+      <div className="name-and-title">
+        <h1 className="my-name">Vladislav Kuznetsov</h1>
+        <h2 className="my-title">Software Developer</h2>
+      </div>
+    </div>
+  )
+}
