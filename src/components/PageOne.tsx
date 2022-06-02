db8aa18fae922dc253dd76a47cb4632bbecc0d63
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2022-06-02 16:48:45 +0200

    added a fancy blob. will render menu on it.

--- src/components/PageOne.tsx
+++ src/components/PageOne.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ReactComponent as Blob } from '../images/blob.svg';
 
 export default function PageOne () {
   return (
@@ -7,6 +8,7 @@ export default function PageOne () {
         <h1 className="my-name">Vladislav Kuznetsov</h1>
         <h2 className="my-title">Software Developer</h2>
       </div>
+        <Blob className="blob" />
     </div>
   )
 }
