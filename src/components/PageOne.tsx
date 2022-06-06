c15abf3b4c5fa2a12903f48f28ee7ae91a7a0998
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2022-06-06 14:03:50 +0200

    removed the blob. changed the location of the navbar. changed background behavior for the first page.

--- src/components/PageOne.tsx
+++ src/components/PageOne.tsx
@@ -2,16 +2,15 @@ import React from 'react';
 import { ReactComponent as Blob } from '../images/blob.svg';
 import Navbar from './Navbar';
 
-export default function PageOne () {
+export default function PageOne() {
   return (
     <div className="page-one">
+      <div className='nav-container'>
+        <Navbar />
+      </div>
       <div className="name-and-title">
         <h1 className="my-name">Vladislav Kuznetsov</h1>
         <h2 className="my-title">Software Developer</h2>
-      </div>
-      <div className='nav-container'>
-        <Navbar />
-        <Blob className="blob" />
       </div>
     </div>
   )
