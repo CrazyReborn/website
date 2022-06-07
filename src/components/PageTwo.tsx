d09985f4c4f613e6b74f2d5db7177f95c528952c
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2022-06-07 14:52:13 +0200

    PageTwo component done. Not resposive atm.

--- src/components/PageTwo.tsx
+++ src/components/PageTwo.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import JhuntScreenshot from '../images/cropped-jhunt-preview.png';
-import AlgoVisScreenshot from '../images/cropped-jhunt-preview.png';
+import HammiltonScreenshot from '../images/screencapture-justdare-github-io-Search-Algorithm-Visualizer-2022-06-06-16_07_58.png';
 import LukasScreenshot from '../images/lightsaber-escape.jpg';
 import ProjectPreview from "./ProjectPreview";
+import '../stylesheets/PageTwo.css';
 
 export default function PageTwo() {
   return (
@@ -12,7 +13,15 @@ export default function PageTwo() {
         <ProjectPreview
           description="Jhunt is a applicaiton managment app that helps me to keep track of the process of looking for a job"
           screenshot={JhuntScreenshot}
-          />
+        />
+        <ProjectPreview
+          description="Hamillton is a simple algorithm visualizer i made to learn algorithms and data structures."
+          screenshot={HammiltonScreenshot}
+        />
+         <ProjectPreview
+          description="Lukas is an experimental game where you can play as a jedi using your smartphone as a lighsaber!"
+          screenshot={LukasScreenshot}
+        />
       </div>
     </div>
   )
--- src/components/ProjectPreview.tsx
+++ src/components/ProjectPreview.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import YTLogo from '../images/yt_logo_mono_dark.png';
 import GHLogo from '../images/GitHub-Mark-Light-64px.png';
+import '../stylesheets/ProjectPreview.css';
 
 interface ProjectInfo {
   description: string,
@@ -14,13 +15,13 @@ export default function ProjectPreview(props: ProjectInfo) {
     <div className="project-preview">
       <img src={screenshot} />
       <article className="project-description">
-        {description}
-      </article>
-      <ul className="links">
-        <a className="live-demo-link" href="#">Live Demo</a>
-        <a className="yt-link" href="#"><img src={YTLogo} /></a>
-        <a className="gh-link" href="#"><img src={GHLogo} /></a>
+        <p>{description}</p>
+        <ul className="links">
+          <a className="live-demo-link" href="#">Live Demo</a>
+          <a className="yt-link" href="#"><img className="yt-logo" src={YTLogo} /></a>
+          <a className="gh-link" href="#"><img className="gh-logo" src={GHLogo} /></a>
       </ul>
+      </article>
     </div>
   )
 }
