c473b06eaf73a58a32cfe363f240fbb4ccb774d1
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2022-06-07 13:19:43 +0200

    remade the first page according to new design. removed the second page.

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import PageOne from './components/PageOne';
-import PageTwo from './components/PageTwo';
 
 function App() {
   return (
     <div className="App">
       <PageOne />
-      <PageTwo />
     </div>
   );
 }
--- src/components/PageOne.tsx
+++ src/components/PageOne.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { ReactComponent as Blob } from '../images/blob.svg';
-import Navbar from './Navbar';
 
 export default function PageOne() {
   return (
     <div className="page-one">
-      <div className='nav-container'>
-        <Navbar />
-      </div>
       <div className="name-and-title">
-        <h1 className="my-name">Vladislav Kuznetsov</h1>
+        <h1 className="my-name">
+          Hello, I am Vladislav
+          <br />
+          Kuznetsov
+        </h1>
+        <span className="bar"></span>
         <h2 className="my-title">Software Developer</h2>
       </div>
     </div>
