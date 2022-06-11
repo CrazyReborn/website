68f98e05f8e110b1c755fcff85905ade11eac557
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2022-06-11 12:49:29 +0200

    added third page with a couple of icons. for now it uses flexbox, but i think a grid would be better.

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import PageOne from './components/PageOne';
 import PageTwo from './components/PageTwo';
+import PageThree from './components/PageThree';
 
 function App() {
   return (
     <div className="App">
       <PageOne />
       <PageTwo />
+      <PageThree />
     </div>
   );
 }
---
+++ src/components/PageThree.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import JsLogo from '../images/js-icon-dark.png';
+import TsLogo from '../images/ts-mono.png';
+import NodeLogo from '../images/nodejs-icon-logo-black-and-white.png';
+import ReactLogo from '../images/react-icon.png';
+import MongoLogo from '../images/mongodb-icon-green.png';
+import ExpressLogo from '../images/express-icon.png';
+import '../stylesheets/PageThree.css';
+
+export default function PageThree () {
+  return (
+    <div className="page-three">
+      <h2>Skillset</h2>
+      <div className="desktop">
+        <img src={JsLogo} />
+        <img src={TsLogo} />
+        <img src={NodeLogo} />
+        <img src={ReactLogo} />
+        <img src={MongoLogo} />
+        <img src={ExpressLogo} />
+      </div>
+      <div className="mobile" >
+        <ul>
+          <li>JavaScript</li>
+          <li>TypeScript</li>
+          <li>React</li>
+          <li>Node</li>
+          <li>Express</li>
+          <li>MongoDB</li>
+          <li>Java</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
